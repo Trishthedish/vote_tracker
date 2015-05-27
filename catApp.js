ff6bba5599de9f2be82b219fc0d6b65a0e2a1ba2
Trish Griego <pmgriego@gmail.com>
2015-05-27 09:18:36 -0700

    Not fully functional but a solid base

---
+++ catApp.js
@@ -0,0 +1,30 @@
+$(function () {
+
+
+var catPic = [
+  'cat1.jpg',
+  'cat2.jpg',
+  'cat3.jpg',
+  'cat4.jpg',
+  'cat5.jpg',
+  'cat6.jpg',
+  'cat7.jpg',
+  'cat8.jpg',
+  'cat9.jpg',
+  'cat10.jpg',
+  'cat11.jpg',
+  'cat12.jpg',
+  'cat13.jpg',
+  'cat14.jpg',
+  ];
+
+$('<img src="vote_tracker/images/catPics/' + "cat" +catPic[Math.floor(Math.random() * catPic.length)] + '"">').appendTo('#leftOpp');
+
+{
+
+
+}
+
+} );
+
+
